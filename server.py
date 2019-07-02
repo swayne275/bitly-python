f0f5b909c90356486f6edd15e7c87c8b83ab57ad
swayne275 <swayne275@gmail.com>
2019-07-01 23:03:56 -0600

    big one - hit bitly api and upgrade for python3

--- server.py
+++ server.py
@@ -7,13 +7,20 @@ import tornado.ioloop
 import tornado.web # !!! SW requirement
 # for http requests
 import requests
+# for url encoding
+import urllib.parse
 
 ##### Define basic error types #####
 generic_internal_err = 1 # generic internal error
 unimplemented_err    = 2 # route or method was not implemented
+invalid_data         = 3 # expected data was not present
 
-api_key = 'cb1da22a1c837ba3f8cd54781461397472cce43e'
-first_url = 'https://api-ssl.bitly.com/v4/user'
+access_token = 'cb1da22a1c837ba3f8cd54781461397472cce43e'
+user_url = 'https://api-ssl.bitly.com/v4/user'
+group_guid = 'Bj71ifpGx2i'
+num_days = 30 # number of days for this problem
+bitlinks_list = []
+
 api_version = "v0.1"
 
 class MainHandler(tornado.web.RequestHandler):
@@ -23,7 +30,7 @@ class MainHandler(tornado.web.RequestHandler):
             response['apiversion'] = api_version
             send_success(self, response)
         except Exception as e:
-            log('Error: could not serve /: ' + e.message)
+            log('Error: could not serve /: ' + str(e))
 
     def post(self):
         handle_unimplemented(self)
@@ -40,15 +47,37 @@ class GenericHandler(tornado.web.RequestHandler):
 class ClickHandler(tornado.web.RequestHandler):
     def get(self):
         try:
+            populate_bitlinks()
             response = {}
             response['sampledata'] = 'hello world'
-            response['jsonresponse'] = request_first()
+            response['bitlinks'] = bitlinks_list
             send_success(self, response)
         except Exception as e:
-            log("Error: could not handle clicks! " + e.message)
+            log("Error: could not handle clicks! " + str(e))
 
-def request_first():
-    r = requests.get(url=first_url)
+def get_group_guid():
+    data = http_get(user_url)
+    if 'default_group_guid' not in data:
+        raise ValueError('default_group_guid not retrieved from Bitly')
+    if not isinstance(data['default_group_guid'], str):
+        raise ValueError('default_group_guid type is invalid from Bitly')
+    return data['default_group_guid']
+
+def populate_bitlinks():
+    data = http_get(get_bitlinks_url())
+    if 'links' not in data:
+        raise ValueError('"links" field not in data retrieved from Bitly')
+    for link_obj in data['links']:
+        if 'link' not in link_obj:
+            raise ValueError('"link" field not in data retrieved from Bitly: ' + json.dumps(link_obj))
+        if not isinstance(link_obj['link'], str):
+            raise ValueError('"link" field data type from Bitly is incorrect')
+        encoded_url = urllib.parse.quote(link_obj['link'])
+        bitlinks_list.append(encoded_url)
+
+def http_get(url):
+    headers = {"Authorization": "Bearer " + access_token}
+    r = requests.get(url=url, headers=headers)
     data = r.json()
     return data
 
@@ -58,7 +87,7 @@ def handle_unimplemented(request_handler):
     try:
         send_httperr(request_handler, unimplemented_err, "Not implemented", status=501)
     except Exception as e:
-        log("Error: Could not respond to unimplemented route/method: " + e.message)
+        log("Error: Could not respond to unimplemented route/method: " + str(e))
 
 def send_success(calling_handler, json_body):
     try:
@@ -66,7 +95,7 @@ def send_success(calling_handler, json_body):
         json_body['uri'] = calling_handler.request.uri
     except Exception as e:
         send_httperr(calling_handler, generic_internal_err,
-            "Error making success response: " + e.message)
+            "Error making success response: " + str(e))
     else:
         calling_handler.write(json_body)
 
@@ -86,13 +115,28 @@ def log(log_msg):
     # !!! SW add file logging possibly
 
 def signal_handler(signal, frame):
-    print ("Ctrl + C!")
+    log("Caught signal, exiting...")
     sys.exit(0)
 
 def server_init():
     log("API version %s running..." % api_version)
     # install signal handler
     signal.signal(signal.SIGINT, signal_handler)
+
+def set_access_token(token):
+    global access_token
+    # verify string and length
+    access_token = token
+
+def set_group_guid():
+    global group_guid
+    group_guid = get_group_guid()
+
+def get_bitlinks_url():
+    return ('https://api-ssl.bitly.com/v4/groups/%s/bitlinks' % group_guid)
+
+def get_countries_url(bitlink):
+    return ('https://api-ssl.bitly.com/v4/bitlinks/%s/countries' % bitlink)
 
 def make_app():
     return tornado.web.Application([
@@ -103,6 +147,11 @@ def make_app():
 
 if __name__ == "__main__":
     try:
+        # maybe grab info first?
+        # verify type, length of access_token
+        set_access_token('cb1da22a1c837ba3f8cd54781461397472cce43e')
+        # verify can get group_guid and type
+        set_group_guid()
         server_init()
         # Start the http webserver
         app = make_app()
@@ -110,4 +159,4 @@ if __name__ == "__main__":
         log("started http server on port 8080")
         tornado.ioloop.IOLoop.current().start()
     except Exception as e:
-        log("Could not start API on port 8080: " + e.message)
+        log("Could not start API on port 8080: " +str(e))
